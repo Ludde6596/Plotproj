1d6c453443012ff717d21f7bc24e767863ca815d
Ludde6596 <61462143+Ludde6596@users.noreply.github.com>
2020-02-25 15:05:30 +0100

    Add files via upload

---
+++ Plotterstyr.asm
@@ -0,0 +1,142 @@
+/*
+ * Plotterstyr.asm
+ *
+ *  Created: 2020-02-25 14:10:53
+ *   Author: ludbe973
+ */ 
+
+ SEKVENS:
+ .db $01, $09, $08, $0A, $02, $06, $04, $05
+
+COLD:
+	ldi r16, HIGH(RAMEND)
+	out SPH, r16
+	ldi r16, LOW(RAMEND)
+	out SPL, r16
+	call HW_INIT
+
+WARM:
+	call YUP
+	call XRIGHT
+	call XLEFT
+	call YDOWN
+STOP:
+jmp STOP
+
+XRIGHT:
+	push r16
+	push r17
+	push ZH
+	push ZL
+	ldi ZH,HIGH(SEKVENS*2)
+	ldi ZL,LOW(SEKVENS*2)
+	ldi r17, $00
+XRIGHT2:
+	lpm r16, Z+
+	out PORTD, r16
+	inc r17
+	call DELAY
+	cpi r17, $08
+	brne XRIGHT2
+	pop ZL
+	pop ZH
+	pop r17
+	pop r16
+	ret
+
+XLEFT:
+	push r16
+	push r17
+	push ZH
+	push ZL
+	ldi ZH,HIGH(SEKVENS*2)
+	ldi ZL,LOW(SEKVENS*2)
+	adiw Z,$07
+	ldi r17, $00
+XLEFT2:
+	lpm r16, Z
+	subi ZL,$01
+	out PORTD, r16
+	inc r17
+	call DELAY
+	cpi r17, $08
+	brne XLEFT2
+	pop ZL
+	pop ZH
+	pop r17
+	pop r16
+	ret
+
+YUP:
+	push r16
+	push r17
+	push ZH
+	push ZL
+	ldi ZH,HIGH(SEKVENS*2)
+	ldi ZL,LOW(SEKVENS*2)
+	ldi r17, $00
+YUP2:
+	lpm r16, Z+
+	out PORTA, r16
+	inc r17
+	call DELAY
+	cpi r17, $08
+	brne YUP2
+	pop ZL
+	pop ZH
+	pop r17
+	pop r16
+	ret
+
+YDOWN:
+	push r16
+	push r17
+	push ZH
+	push ZL
+	ldi ZH,HIGH(SEKVENS*2)
+	ldi ZL,LOW(SEKVENS*2)
+	adiw Z,$07
+	ldi r17, $00
+YDOWN2:
+	lpm r16, Z
+	subi ZL,$01
+	out PORTA, r16
+	inc r17
+	call DELAY
+	cpi r17, $08
+	brne YDOWN2
+	pop ZL
+	pop ZH
+	pop r17
+	pop r16
+	ret
+
+DELAY: ;1ms delay
+	push r16
+	push r17
+	ldi r16, 4
+DELAY1:
+	ldi r17, $FF
+DELAY2:
+	dec r17
+	brne DELAY2
+	dec r16
+	brne DELAY1
+	pop r17
+	pop r16
+	ret
+
+HW_INIT:
+	push r16
+	ldi r16,$FF
+	out DDRA,r16
+	out DDRD,r16
+	ldi r16,$01
+	out DDRB,r16
+
+	sbi DDRB,6 ; slave setup
+	ldi r16,(1<<SPE)
+
+
+	pop r16
+	ret
