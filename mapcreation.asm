dce6e6e86f0d115125c38ebf46edd6b5f3e6d262
Ludde6596 <61462143+Ludde6596@users.noreply.github.com>
2020-03-07 15:32:10 +0100

    Update mapcreation.asm

--- mapcreation.asm
+++ mapcreation.asm
@@ -7,9 +7,9 @@
 
 
 ; Replace with your application code
-  .equ MAPSIZE = 20
-	.equ STEPSIZE = 5
-	.equ ORIGO = 63		;63 riktiga värdet
+  .equ MAPSIZE = 50
+.equ STEPSIZE = 5
+.equ ORIGO = 31		;63 riktiga värdet
 
 .dseg
 .org $0100
@@ -30,8 +30,13 @@ COLD:
 	out SPL,r16
 	call HW_INIT
 WARM:
+	sbis PINB,2
+	rjmp WARM
 	call MAP_CREATION
+	call PEN_DOWN
 	call PLOT_MAP
+	call PEN_UP
+	call RESET_MAP
 FINISH:
 	rjmp FINISH
 
@@ -41,6 +46,9 @@ MAP_CREATION:
 	push r18
 	push ZH
 	push ZL
+	push YH
+	push YL
+	
 	ldi r16,MAPSIZE ;antal loops
 	ldi ZH,HIGH(Y_VAL)
 	ldi ZL,LOW(Y_VAL)
@@ -50,14 +58,15 @@ MAP_1:
 	cpi r16,MAPSIZE
 	breq MAP_2
 RANDOM:
-	call DELAY
 	in r18,TCNT0
-	andi r18,$7F	;and med $7F för att få bort msb 
-	cpi r18,$7E		
+	mov YL,r18
+	ld r18,Y		;Tar utom "random" rader ur minnet
+	andi r18,$3F	;and med $7F för att få bort msb 
+	cpi r18,$3E		
 	brpl R_ADJUST	;kontroll av rand för att den ej ska bli större än 126	
 	rjmp MAP_2
 R_ADJUST:
-	subi r18,$01
+	subi r18,ORIGO
 MAP_2:
 	;set Y_CORD VALUES WITH RANDOM
 	st Z+,r18
@@ -66,6 +75,9 @@ MAP_2:
 	brne MAP_2
 	cpi r16,$00
 	brne MAP_1
+	
+	pop YL
+	pop YH
 	pop ZL
 	pop ZH
 	pop r18
@@ -130,20 +142,50 @@ X_ADJUST:
 	pop r16
 	ret
 
-DELAY: ;RANDOM DELAY
-	push r16
+
+RESET_MAP:
 	push r17
-	in r16,TCNT0
-	andi r16,$03
-DELAY1:
-	ldi r17, $55
-DELAY2:
+	push r18
+	push r19
+
+	lds	r18, X_CORD		;LADDAR x-koordinat från sram
+	lds r17, Y_CORD		;LADDAR y-koordinat från sram
+X_RESET:
+	;;Skicka till plotter
+	ldi r19,$06
+	push r19
+	call SEND
+	pop r19
+	dec r18
+	brne X_RESET
+Y_RESET:
+	cpi r17,ORIGO
+	breq RESET_DONE
+	brpl Y_RESET2
+Y_RESET2:
+	;;Skicka till plotter
+	ldi r19,$01
+	push r19
+	call SEND
+	pop r19
+	inc r17
+	cpi r17,ORIGO
+	brmi Y_RESET2
+	rjmp RESET_DONE
+Y_RESET1:
+	;;Skicka till plotter
+	ldi r19,$02
+	push r19
+	call SEND
+	pop r19
 	dec r17
-	brne DELAY2
-	dec r16
-	brne DELAY1
+	cpi r17,ORIGO
+	brpl Y_RESET1
+RESET_DONE:
+
+	pop r19
+	pop r18
 	pop r17
-	pop r16
 	ret
 
 ;;SEND underprogram finns i både plotterjoy och mapcreation
@@ -169,6 +211,30 @@ WAIT:
 	pop r17
 	pop ZL
 	pop ZH
+	ret
+
+PEN_DOWN:
+	push r19
+	
+	;;Skicka till plotter
+	ldi r19,$03
+	push r19
+	call SEND
+	pop r19
+
+	pop r19
+	ret
+
+PEN_UP:
+	push r19
+	
+	;;Skicka till plotter
+	ldi r19,$05
+	push r19
+	call SEND
+	pop r19
+
+	pop r19
 	ret
 
 HW_INIT:
